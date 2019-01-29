ac93fb6f10372e5aa271adad40ebd7f375188f5e
hanw <hanwfifteen@gmail.com>
2019-01-29 16:26:20 +1300

    w2v2

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -55,14 +55,16 @@ class DishDetail extends Component{
 
     render(){
         return (
-            <div className="row">
-                <div className="col-12 col-md-5 m-1">
-                    {this.renderDish(this.props.selectedDish)}
-                </div>
-                <div className="col-12 col-md-5 m-1">
-                    {this.renderComments((null != this.props.selectedDish) ? this.props.selectedDish.comments : null)} 
-                </div>                           
-            </div>            
+            <div className="container">
+                <div className="row">
+                    <div className="col-12 col-md-5 m-1">
+                        {this.renderDish(this.props.selectedDish)}
+                    </div>
+                    <div className="col-12 col-md-5 m-1">
+                        {this.renderComments((null != this.props.selectedDish) ? this.props.selectedDish.comments : null)} 
+                    </div>                           
+                </div>            
+            </div>           
         );
     }
 }
