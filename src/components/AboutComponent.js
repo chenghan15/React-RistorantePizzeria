baac0c3929a2e875944137a94df7901ff5fdffca
hanw <hanwfifteen@gmail.com>
2019-01-30 17:53:32 +1300

    As2t3

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -2,13 +2,41 @@ import React from 'react';
 import { Breadcrumb, BreadcrumbItem, Card, CardBody, CardHeader, Media } from 'reactstrap';
 import { Link } from 'react-router-dom';
 
-function About(props) {
+function RenderLeader({leaders}){
 
-    const leaders = props.leaders.map((leader) => {
+    const leaderList = leaders.map((leader) => {
         return (
-            <p>Leader {leader.name}</p>
+            <div key={leader.id} className="col-12 mt-5">
+                <Media tag="li">
+                    <Media left middle>
+                        <Media object src={leader.image} alt={leader.name} />
+                    </Media>
+                    <Media body className="ml-5">
+                        <Media heading>{leader.name}</Media>
+                        <p>{leader.designation}</p>
+                        <p>{leader.description}</p>
+                    </Media>
+                </Media>
+            </div>
         );
     });
+
+    return (
+        <div className="col-12">
+            <Media list>
+                {leaderList}
+            </Media>
+        </div>
+    );    
+}
+
+function About(props) {
+
+    // const leaders = props.leaders.map((leader) => {
+    //     return (
+    //         <p>Leader {leader.name}</p>
+    //     );
+    // });
 
     return(
         <div className="container">
@@ -64,11 +92,7 @@ function About(props) {
                 <div className="col-12">
                     <h2>Corporate Leadership</h2>
                 </div>
-                <div className="col-12">
-                    <Media list>
-                        {leaders}
-                    </Media>
-                </div>
+                <RenderLeader leaders={props.leaders}/>
             </div>
         </div>
     );
