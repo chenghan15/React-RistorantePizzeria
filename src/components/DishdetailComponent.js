4b361c4c9275e8b7fd9bb68fb154727044756490
hanw <hanwfifteen@gmail.com>
2019-02-03 17:56:28 +1300

    W4 Fetch v3

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,9 +1,9 @@
 import React from 'react';
-
 import {Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import CommentForm from './CommentForm';
 import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
 
     function RenderComments({arrComm, addComment, dishId}){        
         if(null != arrComm)
@@ -39,11 +39,11 @@ import { Loading } from './LoadingComponent';
         if(dish != null){
             return ( 
                 <Card>
-                    <CardImg width="100%" src={dish.image} alt={dish.name} />
-                    <CardBody>
-                        <CardTitle>{dish.name}</CardTitle>
-                        <CardText>{dish.description}</CardText>
-                    </CardBody>
+                <CardImg width="100%" src={baseUrl + dish.image} alt={dish.name} />
+                <CardBody>
+                    <CardTitle>{dish.name}</CardTitle>
+                    <CardText>{dish.description}</CardText>
+                </CardBody>
                 </Card>               
             );
         }
